efa9c43051159c7dfaf549f9f3bce4f5d83f191b
lsiemion <01178436@pw.edu.pl>
2023-11-13 12:06:26 +0100

    add ALU logic

--- ALU.v
+++ ALU.v
@@ -15,4 +15,30 @@ localparam NOT_EVEN_1 = 2;
 localparam ZEROS = 1;
 localparam OVERFLOW = 0;
 
+always @(posedge clk or negedge i_reset) begin
+    if (!i_reset) begin
+        // state reset
+        o_result <= 0;
+        o_status <= 0;
+    end else begin
+        // status reset
+        o_status <= 0;
+
+        case (iop)
+            4'b0000: begin // Operation example A <~B
+            end
+            4'b0001: begin // Operation example AS~B
+            end
+            4'b0010: begin // Operation example A/B
+            end
+            4'b0011: begin // Operation example ZM(A) => U2(A)
+            end
+            default: begin
+                o_status[ERROR] <= 1'b1; // Unknown operations
+            end
+        endcase
+    end
+end
+
+
 endmodule
