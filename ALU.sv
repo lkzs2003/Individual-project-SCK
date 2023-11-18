e472f355373fe9fef83e9093d81db519a6dce9cd
lsiemion <01178436@pw.edu.pl>
2023-11-18 12:31:34 +0100

    removing duplicate code

--- ALU.sv
+++ ALU.sv
@@ -1,28 +1,3 @@
-module sync_arith_unit_29 #(parameter M = 32) (
-    input wire [M-1:0] iarg_A,
-    input wire [M-1:0] iarg_B,
-    input wire [3:0] iop,
-    input wire clk,
-    input wire i_reset,
-    output reg [M-1:0] o_result,
-    output reg [3:0] o_status
-);
-
-
-// Status definitions
-localparam ERROR = 3;
-localparam NOT_EVEN_1 = 2;
-localparam ZEROS = 1;
-localparam OVERFLOW = 0;
-
-always @(posedge clk or negedge i_reset) begin
-    if (!i_reset) begin
-        // state reset
-        o_result <= 0;
-        o_status <= 0;
-    end else begin
-        // status reset
-        o_status <= 0;
 // Module sync_arith_unit_29: A Synchronous Arithmetic and Logic Unit
 module sync_arith_unit_29 #(parameter M = 32) (
     input wire [M-1:0] iarg_A,    // Input argument A, M-bit wide
