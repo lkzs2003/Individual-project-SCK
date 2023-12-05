9dabe3e91c0c84286102b1fdaf147a30166276d2
lsiemion <01178436@pw.edu.pl>
2023-12-05 20:29:55 +0100

    bug fixes

--- sync_arith_unit_29.sv
+++ sync_arith_unit_29.sv
@@ -1,77 +1,66 @@
-`define OP_SHIFT 2'b00
-`define OP_ADD   2'b01
-`define OP_DIV   2'b10
-`define OP_U2    2'b11
+`timescale 1ns / 1ps
 
-module sync_arith_unit_29 #(parameter M = 8) (
-    input wire [1:0] i_op,
-    input wire [M-1:0] i_arg_A,
-    input wire [M-1:0] i_arg_B,
-    input wire i_clk,
-    input wire i_reset,
-    output reg [M-1:0] o_result,
-    output reg [3:0] o_status
+module sync_arith_unit_29 #(parameter M = 32) (
+    input wire [M-1:0] iarg_A,    // Input argument A, M-bit wide
+    input wire [M-1:0] iarg_B,    // Input argument B, M-bit wide
+    input wire [3:0] iop,         // Operation code input, 4-bit wide
+    input wire clk,               // Clock input
+    input wire i_reset,           // Synchronous reset input
+    output reg [M-1:0] o_result,  // Output result, M-bit wide
+    output reg [3:0] o_status     // Output status, 4-bit wide
 );
 
-    // Status flags definitions
-    localparam ERROR = 3;
-    localparam ZEROS = 1;
-    localparam OVERFLOW = 0;
+// Status flags definitions
+localparam ERROR = 3;        // Error flag
+localparam NOT_EVEN_1 = 2;   // Flag for odd number of ones in the result
+localparam ZEROS = 1;        // Flag indicating all zeros in the result
+localparam OVERFLOW = 0;     // Overflow flag
 
-    always @(posedge i_clk or negedge i_reset) begin
-        if (!i_reset) begin
-            // Resetting the module
-            o_result <= 0;
-            o_status <= 0;
-        end else begin
-            // Resetting the status at the start of each cycle
-            o_status <= 0;
+// ALU logic implementation
+always @(posedge clk or negedge i_reset) begin
+    if (!i_reset) begin
+        // Resetting the module
+        o_result <= 0;
+        o_status <= 0;
+    end else begin
+        // Resetting the status at the start of each cycle
+        o_status <= 0;
 
-            case (i_op)
-                `OP_SHIFT: begin
-                    int signed_arg_B = $signed(i_arg_B); // Convert to signed for proper shift
-                    if (signed_arg_B < 0) begin
-                        o_status[ERROR] <= 1'b1;
-                        o_result <= {M{1'bx}}; // Undefined value
-                    end else begin
-                        o_result <= i_arg_A >> signed_arg_B;
-                    end
+        case (iop)
+            4'b0000: begin // Operation A<~B (Bitwise Right Shift)
+                if (~iarg_B < 0) begin
+                    o_status[ERROR] <= 1'b1;
+                    o_result <= {M{1'bx}}; // Undefined value
+                end else begin
+                    o_result <= iarg_A >> ~iarg_B;
                 end
-
-                `OP_ADD: begin
-                    o_result <= i_arg_A + ~i_arg_B;
-                    // Check for overflow
-                    if (^o_result[M-1:0] != o_result[M-1]) begin
-                        o_status[OVERFLOW] <= 1'b1;
-                    end
-                end
-
-                `OP_DIV: begin
-                    if (i_arg_B == 0) begin
-                        o_status[ERROR] <= 1'b1;
-                        o_result <= {M{1'bx}};
-                    end else begin
-                        o_result <= i_arg_A / i_arg_B;
-                    end
-                end
-
-                `OP_U2: begin
-                    if (i_arg_A[M-1] == 1'b0) begin
-                        o_result <= i_arg_A;
-                    end else begin
-                        o_result <= (~i_arg_A + 1'b1);
-                    end
-                end
-
-                default: begin
-                    o_status[ERROR] <= 1'b1; // Unknown operation
-                end
-            endcase
-
-            // Additional status checks
-            if (o_result == 0) begin
-                o_status[ZEROS] <= 1'b1;
             end
-        end
+            4'b0001: begin // Operation AS~B (Check if A <= ~B)
+                o_result <= (iarg_A <= ~iarg_B) ? {M{1'b1}} : {M{1'b0}};
+            end
+            4'b0010: begin // Operation A/B (Division)
+                if (iarg_B == 0) begin
+                    o_status[ERROR] <= 1'b1;
+                    o_result <= {M{1'bx}}; // Undefined value
+                end else begin
+                    int signed_dividend = $signed(iarg_A);
+                    int signed_divisor = $signed(iarg_B);
+                    int signed_result = signed_dividend / signed_divisor;
+                    o_result <= signed_result;
+                end
+            end
+            4'b0011: begin // Operation ZM(A) => U2(A) (Code Conversion)
+                if (iarg_A[M-1] == 1'b0) begin // Positive number
+                    o_result <= iarg_A;
+                end else begin // Negative number
+                    o_result <= (~iarg_A + 1'b1) | (1'b1 << (M-1)); // Convert to Two's Complement
+                end
+            end
+            default: begin
+                o_status[ERROR] <= 1'b1; // Unknown operation
+            end
+        endcase
     end
+end
+
 endmodule
