e3819e1a7e17247b989d4e93989e9001791c7d14
lsiemion <01178436@pw.edu.pl>
2023-12-09 19:36:30 +0100

    change of division module

--- sync_arith_unit_29.sv
+++ sync_arith_unit_29.sv
@@ -1,81 +1,33 @@
-`include "divider.sv"
-
 `timescale 1ns / 1ps
 
-module sync_arith_unit_29 #(parameter M = 32) (
-    input wire [M-1:0] iarg_A,
-    input wire [M-1:0] iarg_B,
-    input wire [3:0] iop,
-    input wire clk,
-    input wire i_reset,
-    output reg [M-1:0] o_result,
-    output reg [3:0] o_status
-);
-// Definicje operacji
-localparam OP_BITWISE_SHIFT = 4'b0000;
-localparam OP_COMPARE_AS = 4'b0001;
-localparam OP_DIVIDE = 4'b0010;
-localparam OP_ZM_TO_U2 = 4'b0011;
-
-// Status flags definitions
-localparam ERROR = 3;
-localparam NOT_EVEN_1 = 2;
-localparam ZEROS = 1;
-localparam OVERFLOW = 0;
-
-reg [M-1:0] remainder;
-integer i;
-
-// Instancja modułu divider
-wire [M-1:0] div_quotient, div_remainder;
-wire div_error;
-divider #(.M(M)) div_unit (
-    .dividend(iarg_A),
-    .divisor(iarg_B),
-    .quotient(div_quotient),
-    .remainder(div_remainder),
-    .error(div_error)
+module divider #(parameter M = 32) (
+    input wire signed [M-1:0] dividend,  // Make inputs signed to handle negative numbers
+    input wire signed [M-1:0] divisor,
+    output reg signed [M-1:0] quotient,
+    output reg signed [M-1:0] remainder
 );
 
-always @(posedge clk or negedge i_reset) begin
-    if (!i_reset) begin
-        o_result <= 0;
-        o_status <= 0;
-    end else begin
-        o_status <= 0;
+    integer i;
+    reg signed [2*M-1:0] temp_dividend;  // Make temp_dividend signed
 
-        case (iop)
-            OP_BITWISE_SHIFT: begin // Operation A<~B (Bitwise Right Shift)
-                if (~iarg_B < 0) begin
-                    o_status[ERROR] <= 1'b1;
-                    o_result <= {M{1'bx}}; // Niezdefiniowana wartość
-                end else begin
-                    o_result <= iarg_A >> (~iarg_B & (M-1));
+    always @* begin  // Use always @* for sensitivity list based on used signals
+        quotient = 0;
+        remainder = 0;
+        temp_dividend = {{M{1'b0}}, dividend};
+
+        if (divisor != 0) begin
+            for (i = M-1; i >= 0; i = i - 1) begin
+                remainder = (remainder << 1) | temp_dividend[2*M-1];
+                temp_dividend = temp_dividend << 1;
+
+                if (remainder >= 0) begin  // Compare with 0 instead of divisor
+                    remainder = remainder - divisor;
+                    quotient[i] = 1;
                 end
             end
-            OP_COMPARE_AS: begin // Operation AS~B (Check if A <= ~B)
-                o_result <= (iarg_A <= ~iarg_B) ? 32'b1 : 32'b0;
-            end
-            OP_DIVIDE: begin // Operation A/(-B) (Division)
-                if (-iarg_B == 0) begin
-                    o_status[ERROR] <= 1'b1;
-                    o_result <= {M{1'bx}};
-                end else begin
-                    o_result <= div_quotient; // Wynik dzielenia z modułu divider
-                end
-            end
-            OP_ZM_TO_U2: begin // Operation ZM(A) => U2(A) (Code Conversion)
-                if (iarg_A[M-1]) begin
-                    o_result <= (~iarg_A + 1); // Konwersja liczby ujemnej
-                end else begin
-                    o_result <= iarg_A; // Liczba dodatnia pozostaje bez zmian
-                end
-            end
-            default: begin
-                o_status[ERROR] <= 1'b1;
-            end
-        endcase
+        end else begin
+            quotient = {M{1'bx}};
+            remainder = {M{1'bx}};
+        end
     end
-end
-
 endmodule
--- sync_arith_unit_29_tb.sv
+++ sync_arith_unit_29_tb.sv
@@ -1,72 +1,75 @@
 `include "sync_arith_unit_29.sv"
 
-
 `timescale 1ns / 1ps
 
 module sync_arith_unit_29_tb;
 
-parameter M = 32;
-reg [M-1:0] iarg_A, iarg_B;
-reg [3:0] iop;
-reg clk, i_reset;
-wire [M-1:0] o_result;
-wire [3:0] o_status;
+    parameter M = 32;
+    reg [M-1:0] iarg_A, iarg_B;
+    reg [3:0] iop;
+    reg clk, i_reset;
+    wire [M-1:0] o_result;
+    wire [3:0] o_status;
 
-sync_arith_unit_29 #(.M(M)) UUT (
-    .iarg_A(iarg_A),
-    .iarg_B(iarg_B),
-    .iop(iop),
-    .clk(clk),
-    .i_reset(i_reset),
-    .o_result(o_result),
-    .o_status(o_status)
-);
+    sync_arith_unit_29 #(.M(M)) UUT (
+        .iarg_A(iarg_A),
+        .iarg_B(iarg_B),
+        .iop(iop),
+        .clk(clk),
+        .i_reset(i_reset),
+        .o_result(o_result),
+        .o_status(o_status)
+    );
 
-initial begin
-    // Initialization
-    $dumpfile("waveform.vcd");
-    $dumpvars(0, sync_arith_unit_29_tb);
-    clk = 0;
-    i_reset = 1;
-    iarg_A = 0;
-    iarg_B = 0;
-    iop = 0;
+    always #5 clk = ~clk;
 
-    // Reset
-    #10;
-    i_reset = 0;
-    #10;
-    i_reset = 1;
-    #10;
+    initial begin
+        clk = 0;
+        i_reset = 1'b1;
+        iarg_A = 0;
+        iarg_B = 0;
+        iop = 0;
 
-    // Test Cases
-    // Test Case 1: Bitwise Right Shift
-    iarg_A = 15; iarg_B = 3; iop = 4'b0000;
-    #20;
+        // Reset
+        #10 i_reset = 1'b0;
+        #20 i_reset = 1'b1;
 
-    // Test Case 2: Check if A <= ~B
-    iarg_A = 10; iarg_B = 5; iop = 4'b0001;
-    #20;
+        // Testy operacji przesunięcia bitowego
+        performTest(32'b11111111111111110000000000000000, 32'b00000000000000000000000000000100, 4'b0000);
+        performTest(32'b10101010101010100101010101010101, 32'b00000000000000000000000000001000, 4'b0000);
+        performTest(32'b00010010001101000101011001111000, 32'b00000000000000000000000000010000, 4'b0000);
 
-    // Test Case 3: Division A / ~B
-    iarg_A = 20; iarg_B = 4; iop = 4'b0010;
-    #20;
+        // Testy operacji porównania
+        performTest(32'b00000000000000000000000000001010, 32'b00000000000000000000000000000101, 4'b0001);
+        performTest(32'b00000000000000000000000000010100, 32'b00000000000000000000000000011001, 4'b0001);
+        performTest(32'b00000000000000000000000000001111, 32'b00000000000000000000000000001111, 4'b0001);
 
-    // Test Case 4: Conversion ZM(A) => U2(A)
-    iarg_A = 8'b10000001; iop = 4'b0011;
-    #20;
+        // Testy operacji dzielenia
+        performTest(32'b00000000000000000000000001100100, 32'b00000000000000000000000000001010, 4'b0010);
+        performTest(32'b00000000000000000000000000110010, 32'b00000000000000000000000000000101, 4'b0010);
+        performTest(32'b00000000000000000000000011001000, 32'b00000000000000000000000000010100, 4'b0010);
 
-    #100; // Wait for some time to observe the results
-    $finish; // Finish simulation
-end
+        // Testy operacji konwersji ZM -> U2
+        performTest(32'b10000000000000000000000000000001, 32'b00000000000000000000000000000000, 4'b0011);
+        performTest(32'b01111111111111111111111111111111, 32'b00000000000000000000000000000000, 4'b0011);
+        performTest(32'b10000000000000010010001101000101, 32'b00000000000000000000000000000000, 4'b0011);
 
-// Generating a clock signal
-always #5 clk = ~clk;
+        // Zakończenie symulacji
+        $finish;
+    end
 
-// Monitor values
-initial begin
-    $monitor("Time = %t, iarg_A = %d, iarg_B = %d, iop = %b, o_result = %d, o_status = %b",
-             $time, iarg_A, iarg_B, iop, o_result, o_status);
-end
+    task performTest;
+        input [M-1:0] argA, argB;
+        input [3:0] op;
+        begin
+            iarg_A <= argA;
+            iarg_B <= argB;
+            iop <= op;
+            #10;
+            $display("Test: iop=%b, iarg_A=%b, iarg_B=%b, o_result=%b, o_status=%b",
+                     op, argA, argB, o_result, o_status);
+            #10;
+        end
+    endtask
 
 endmodule
